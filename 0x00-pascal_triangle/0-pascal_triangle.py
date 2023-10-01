d715fc152508a074bfb81ef6e227d62d609a5427
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-10-02 01:11:10 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
+'''A module for pascal's triangle
+'''
+
 def pascal_triangle(n):
+  '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer based on arg int n
+    '''
   if n <= 0 or type(n) not is int:
     return []
   result = [[1]]
