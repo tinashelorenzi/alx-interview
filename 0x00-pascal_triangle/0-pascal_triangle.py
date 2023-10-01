6038076514e90045efc30fa4e699f4528df77061
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-10-01 23:17:19 +0000

    pycodestyle fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,18 +2,19 @@
 '''A module for pascal's triangle
 '''
 
+
 def pascal_triangle(n):
-  '''Creates a list of lists of integers representing
+    '''Creates a list of lists of integers representing
     the Pascal's triangle of a given integer based on arg int n
     '''
-  if n <= 0 or type(n) not is int:
-    return []
-  result = [[1]]
-  for i in range(1, n):
-    prev_row = result[-1]
-    row = [1]
-    for j in range(len(prev_row) - 1):
-      row.append(prev_row[j] + prev_row[j + 1])
-    row.append(1)
-    result.append(row)
-  return result
+    if n <= 0 or type(n) not is int:
+        return []
+    result = [[1]]
+    for i in range(1, n):
+        prev_row = result[-1]
+        row = [1]
+        for j in range(len(prev_row) - 1):
+            row.append(prev_row[j] + prev_row[j + 1])
+        row.append(1)
+        result.append(row)
+    return result
