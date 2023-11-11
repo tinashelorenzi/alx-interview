c4d4ef1ee573b80da752822d92abe00f3f554d01
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-11-11 15:23:18 +0000

    CD

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,17 +4,9 @@ import sys
 def is_safe(board, row, col, N):
     # Check if there is a queen in the same row to the left
     for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Check if there is a queen in the upper diagonal on the left
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check if there is a queen in the lower diagonal on the left
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
+        if board[i] == row or \
+           board[i] == row - (col - i) or \
+           board[i] == row + (col - i):
             return False
 
     return True
@@ -23,16 +15,13 @@ def is_safe(board, row, col, N):
 def solve_nqueens_util(board, col, N):
     if col == N:
         # All queens are placed, print the solution
-        for i in range(N):
-            print(" ".join(map(str, board[i])))
-        print()
+        print([[i, board[i]] for i in range(N)])
         return
 
     for i in range(N):
         if is_safe(board, i, col, N):
-            board[i][col] = 1
+            board[col] = i
             solve_nqueens_util(board, col + 1, N)
-            board[i][col] = 0  # backtrack
 
 
 def solve_nqueens(N):
@@ -46,8 +35,7 @@ def solve_nqueens(N):
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0] * N for _ in range(N)]
-    solve_nqueens_util(board, 0, N)
+    solve_nqueens_util([0] * N, 0, N)
 
 
 if __name__ == "__main__":
