99b9f404e7d8dde66620ffa04e2bbfed52f28a89
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-11-19 05:54:35 +0000

    fixes

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,11 +6,11 @@
 def rotate_2d_matrix(matrix):
     """Rotates an m by n 2D matrix in place.
     """
-    if type(matrix) is list:
+    if not isinstance(matrix, list):
         return
     if len(matrix) <= 0:
         return
-    if not all(map(lambda x: type(x) is list, matrix)):
+    if not all(map(lambda x: isinstance(x, list), matrix)):
         return
     rows = len(matrix)
     cols = len(matrix[0])
