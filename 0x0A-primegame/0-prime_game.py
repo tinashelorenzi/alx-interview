50613e83bb3da744112b979ee22b251fd37dd778
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-12-10 20:36:49 +0000

    Fixes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,9 +20,9 @@ def isWinner(x, nums):
             primes[j - 1] = False
     # filter the number of primes less than n in nums for each round
     for _, n in zip(range(x), nums):
-        primes_count = sum(1 for prime in primes[:n] if prime)
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
         bens_wins += primes_count % 2 == 0
-        marias_wins += primes_count % 2
+        marias_wins += primes_count % 2 == 1
     if marias_wins == bens_wins:
         return None
     return 'Maria' if marias_wins > bens_wins else 'Ben'
--- 0x0A-primegame/README.md
+++ 0x0A-primegame/README.md
@@ -1,5 +1,6 @@
 .
 ├── 0-prime_game.py
+├── 0-prime_game_test.txt
 └── README.md
 
-0 directories, 2 files
+0 directories, 3 files
